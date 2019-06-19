b55b6897e9a35530b41c07d6fdde56d4f92a55c3
Stella McEwen <mcewen.stella@gmail.com>
2019-06-19 23:06:21 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,49 @@
 class Pokemon
-end
+
+attr_accessor :id, :name, :type, :db, :hp
+
+   def initialize(id: nil, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+
+
+   def self.find(id, db)
+     sql = <<-SQL 
+     SELECT * 
+     FROM pokemon 
+     WHERE id = ?
+     SQL
+    
+    db.execute(sql, id).map do |row|
+      Pokemon.new(id: row[0], name: row[1], type: row[2], hp: row[3], db: db)
+    end.first 
+  end
+
+
+   def self.save(name, type, db)
+     sql = <<-SQL 
+     INSERT INTO pokemon (name, type)
+     VALUES (?,?)
+     SQL
+     
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
+
+   def alter_hp(new_hp, db)
+     sql = <<-SQL 
+     UPDATE pokemon  
+     SET hp = ?
+     WHERE id = ? 
+     SQL
+    db.execute(sql, new_hp, self.id)
+  end
+
+
+
+ end	
