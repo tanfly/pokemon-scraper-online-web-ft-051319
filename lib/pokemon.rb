ad4cfee0d3528a09398f40d8053273f9415e1a29
Stella McEwen <mcewen.stella@gmail.com>
2019-06-19 22:53:43 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,6 +12,7 @@ attr_accessor :id, :name, :type, :db, :hp
 
 
    def self.find(id, db)
+     sql = <<-SQL
     db.execute("SELECT * FROM pokemon WHERE id=?", id).map do |row|
       Pokemon.new(id: row[0], name: row[1], type: row[2], hp: row[3], db: db)
     end.first 
