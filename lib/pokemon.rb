fa17bb4e059d785db29e1a0d07c77b2ddff88f93
Stella McEwen <mcewen.stella@gmail.com>
2019-06-19 22:19:49 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
   
-  def initialize(id, name, type, db)
+  def initialize(name)
     @id = id
     @name = name
     @type = type
