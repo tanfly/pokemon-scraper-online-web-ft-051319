ddc6f4a38e1d21a0aee99794f07de796f89ffb65
Stella McEwen <mcewen.stella@gmail.com>
2019-06-19 22:14:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL 
+    INSERT INTO pokemon (name, type)
+    VALUES (?, ?)
+    SQL
+    
+    db.execute(sql, name, type)
+    
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL 
+    SELECT * 
+    FROM pokemon 
+    WHERE id = ? 
+    SQL
+    db.execute(sql, id).map do |row| 
+      Pokemon.new(@id = row[0], @name = row[1], @type = row[2], @db = db)
+  end.first 
+end
+  
 end
