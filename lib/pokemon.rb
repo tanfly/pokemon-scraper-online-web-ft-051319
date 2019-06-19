7383846fed7aafd08a53ea725606c79736b46b73
Stella McEwen <mcewen.stella@gmail.com>
2019-06-19 22:17:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -29,7 +29,7 @@ class Pokemon
     WHERE id = ? 
     SQL
     db.execute(sql, id).map do |row| 
-      Pokemon.new(id: row[0], name: row[1], @type = row[2], @db = db)
+      Pokemon.new(id: row[0], name: row[1], type: row[2], @db = db)
   end.first 
 end
   
